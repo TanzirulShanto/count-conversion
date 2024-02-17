d7b8a248ab62f3a7bb8fb3803ec10e6fb5e492a8
TanzirulShanto <tanzirulislam025@gmail.com>
2024-02-18 03:21:50 +0600

    Third Commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/convert', (req, res)=>{
-    res.render('counts/main', {data: false});
+    res.render('counts/main', {data: false, isCountZero: false});
 });
 
 
@@ -32,43 +32,55 @@ app.post('/convert', (req, res)=>{
     const count = req.body.count;
     const system = req.body.system;
 
+    if(!count){
+        return res.render('counts/main', {data: false, isCountZero: true});
+    }
     switch (system) {
         case "tex":
-            res.render('counts/main', {data: texToAll(count), counter: 1});
+            res.render('counts/main', {data: texToAll(count), counter: 1, isCountZero: false});
             break;
         case "den":
-            res.render('counts/main', {data: denToAll(count), counter: 1});
+            res.render('counts/main', {data: denToAll(count), counter: 1, isCountZero: false});
             break;
         
         case "ne":
-            res.render('counts/main', {data: neToAll(count), counter: 1});
+            res.render('counts/main', {data: neToAll(count), counter: 1, isCountZero: false});
             break;
 
         case "new":
-            res.render('counts/main', {data: newToAll(count), counter: 1});
+            res.render('counts/main', {data: newToAll(count), counter: 1, isCountZero: false});
             break;
 
         case "nes":
-            res.render('counts/main', {data: nesToAll(count), counter: 1});
+            res.render('counts/main', {data: nesToAll(count), counter: 1, isCountZero: false});
             break;
 
         case "nel":
-            res.render('counts/main', {data: nelToAll(count), counter: 1});
+            res.render('counts/main', {data: nelToAll(count), counter: 1, isCountZero: false});
             break;
         
         case "lbps":
-            res.render('counts/main', {data: lbpsToAll(count), counter: 1});
+            res.render('counts/main', {data: lbpsToAll(count), counter: 1, isCountZero: false});
             break;
         case "nm":
-            res.render('counts/main', {data: nmToAll(count), counter: 1});
+            res.render('counts/main', {data: nmToAll(count), counter: 1, isCountZero: false});
             break;
             
         default:
-            res.render('counts/main', {data: false});
+            res.render('counts/main', {data: false, isCountZero: false});
     }
 });
 
 
+app.use((req, res, next) => {
+    res.render('err', {message:"Invalid Route", statusCode: 404});
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        message: "Server Broke!!!"
+    });
+});
 
 
 module.exports = app;
